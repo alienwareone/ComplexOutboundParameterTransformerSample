2d1885c1a75107bf230685c8bfae98cba9043396
alienwareone <beni@mailboxes.ch>
2020-11-10 19:33:22 +0100

    UrlGeneration comments

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -13,7 +13,15 @@ namespace WebApplication2.Controllers
                 Category = "Smartphone",
                 SearchQuery = "dual sim"
             };
+
             var url = Url.RouteUrl("ProductIndex", new { filters = context });
+            // url == "/Germany/Smartphone%3Fq%3Ddual%20sim"
+            // Should be: "/Germany/Smartphone?q=dual%20sim"
+
+            var url2 = Url.RouteUrl("ProductIndex", new { filters = context, x = 1, y = 2, z = 3 });
+            // url2 == "/Germany/Smartphone%3Fq%3Ddual%20sim?x=1&y=2&z=3"
+            // This should be merged to: "/Germany/Smartphone?q=dual%20sim&x=1&y=2&z=3"
+
             return Content(url);
         }
     }
